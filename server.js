3070321f614e3fa63fd1ad16b2c3920e880a3e42
Carlos <perez.carlos_a@yahoo.com>
2021-02-01 14:13:15 -0800

    env changes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const express = require('express');
-const PORT = process.env.MONGODB_URI || 3001;
+
+const PORT = process.env.MONGODB_URI || 27016;
 
 const app = express();
 
@@ -17,8 +18,6 @@ app.use(express.static('public'));
 // Connecting to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
 	useFindAndModify: false,
 });
 
