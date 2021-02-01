540a201ddf20bc543f0d3ebb8cfcbc9fcb6793be
Carlos <perez.carlos_a@yahoo.com>
2021-01-31 21:11:42 -0800

    added mongodb Atlas connection

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connecting to MongoDB
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 	useFindAndModify: false,
 });
 
