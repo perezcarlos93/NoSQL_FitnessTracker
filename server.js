a6bd1289274ec744703af2489a2eace35eda5800
Carlos <perez.carlos_a@yahoo.com>
2021-02-01 13:26:06 -0800

    removed local ports

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const express = require('express');
-const PORT = 3001;
+// const PORT = 3001;
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connecting to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
@@ -25,6 +25,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 app.use(require('./routes/homeRoutes'));
 app.use(require('./routes/dataRoutes'));
 
-app.listen(process.env.MONGODB_URI || PORT, () => {
+app.listen(process.env.MONGODB_URI, () => {
 	console.log(`App running on http://localhost:${PORT}`);
 });
