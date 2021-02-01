b777eee94cc8cff4d2db2acc49e25644b4ca015f
Carlos <perez.carlos_a@yahoo.com>
2021-02-01 14:38:59 -0800

    set heroku variable

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+// require('dotenv').config();
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const express = require('express');
 
-const PORT = process.env.MONGODB_URI || 27016;
+const PORT = process.env.MONGODB_URI || 3001;
 
 const app = express();
 
@@ -16,14 +16,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connecting to MongoDB
-mongoose.connect(
-	'mongodb+srv://perezcarlos93:8AA3gHyg8QdwmtGi@cluster0.arzwt.mongodb.net/testbd?retryWrites=true&w=majority' ||
-		'mongodb://localhost/workout',
-	{
-		useNewUrlParser: true,
-		useFindAndModify: false,
-	}
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+});
 
 // Routes
 app.use(require('./routes/homeRoutes'));
